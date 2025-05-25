eafd7877f51cd192c8929295e8192882a6f5bf15
Batuhan Aydin <bados5561@gmail.com>
2025-05-25 16:16:55 +0300

    Fourier Dönüşümü algoritmaları eklendi.

--- algoritma.py
+++ algoritma.py
@@ -34,6 +34,42 @@ def sesi_kaydet(path, orneklemeler, ornekleme_orani):
     except Exception as e:
         raise Exception(f"{path} dosyasina yazarken hata meydana geldi: {e}")
 
+def stft(ses_sinyali, pencere_boyutu = 2048, kaydirma_sayisi = 512):
+    pencere_boyutu = min(pencere_boyutu, len(ses_sinyali))
+    pencere = np.hanning(pencere_boyutu)
+    cerceve_sayisi = 1 + int((len(ses_sinyali) - pencere_boyutu) / kaydirma_sayisi)
+    cerceve_sayisi = max(1,cerceve_sayisi)
+    stft_matrix = np.zeros((pencere_boyutu // 2 + 1, cerceve_sayisi), dtype=np.complex64)
+    for i in range(cerceve_sayisi):
+        baslangic = i * kaydirma_sayisi
+        bitis = min(baslangic + pencere_boyutu, len(ses_sinyali))
+        if bitis - baslangic < pencere_boyutu:
+            cerceve = np.zeros(pencere_boyutu)
+            cerceve[:bitis - baslangic] = ses_sinyali[baslangic:bitis]
+        else:
+            cerceve = ses_sinyali[baslangic:bitis]
+            cerceve = cerceve * pencere
+            spektrum = np.fft.rfft(cerceve)
+            stft_matrix[:, i] = spektrum
+    return stft_matrix
+
+def istft(stft_matrix, pencere_boyutu = 2048, kaydirma_sayisi = 512):
+    pencere = np.hanning(pencere_boyutu)
+    cerceve_sayisi = stft_matrix.shape[1]
+    ses_sinyali_length = (cerceve_sayisi - 1) * kaydirma_sayisi + pencere_boyutu
+    ses_sinyali = np.zeros(ses_sinyali_length)
+    for i in range(cerceve_sayisi):
+        baslangic = i * kaydirma_sayisi
+        bitis = baslangic + pencere_boyutu
+        cerceve = np.fft.irfft(stft_matrix[:, i])
+        if len(cerceve) < pencere_boyutu:
+            temp = np.zeros(pencere_boyutu)
+            temp[:len(cerceve)] = cerceve
+            cerceve = temp
+        ses_sinyali[baslangic:bitis] += cerceve * pencere[:len(cerceve)]
+    return ses_sinyali
+
+
 
 
 
